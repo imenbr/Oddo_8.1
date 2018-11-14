e29c976fb5c0153c3bb5a94b071c1c00360d3b04
imenbr <imen.bradai.ib@gmail.com>
2018-11-14 12:17:51 +0100

    help

---
+++ customerAddons/module_test/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'test',
+    'version': '1.0',
+    'category': 'test',
+    'description': """test""",
+    # 'website': 'https://www.odoo.com/page/accounting',
+    'depends': ['base'],
+    'data': [        'openacademy.xml'    ],
+    'installable': True,
+}
